5afdd44e8f68a26710c5c4db192a7ffe866bc5f0
schaubce <schaubce@uwec.edu>
2014-03-03 10:47:58 -0600

    First commit/first static page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskApp::Application.config.secret_token = '11d82c0e7a024c1384c4c17359e05c5178059200bcfa2d208278b36f59eaff91c3307bd6ca34f5bbc702db102d54ebe9f536ff56a3296f7d9b8179da2f8de365'
