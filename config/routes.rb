e20501267fe8d3812d99ab693771cdf7dadb22eb
schaubce <schaubce@uwec.edu>
2014-03-05 15:56:24 -0600

    User controller/model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 TaskApp::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
+
+  root :to=> 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
